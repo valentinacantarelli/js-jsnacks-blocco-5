52c6713f5cabfbefd19a235b11fafedf04fc7bf4
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-08 14:35:20 +0200

    parola al contrario

--- snack3/script.js
+++ snack3/script.js
@@ -1,15 +1,15 @@
 // Scrivi una funzione che accetti una stringa come argomento e la ritorni
 // girata (es. Ciao -> oaiC)
-var nuovaParola;
+
 
 function reverse(parola){
-   
+    var nuovaParola="";
     for(var i = parola.length-1; i>=0;i--){
-       nuovaParola = nuovaParola+parola[i];
+       nuovaParola +=parola[i];
     }
     return nuovaParola;
 }
 
-var parolaUtente=prompt("Ciao,inserisci una parola");
+var parolaUtente=reverse(prompt("Ciao,inserisci una parola"));
 
-console.log(reverse(parolaUtente));
+console.log(parolaUtente);
