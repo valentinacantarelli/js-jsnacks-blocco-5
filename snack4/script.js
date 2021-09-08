4cdc62f750b328608c5d58edf523357cdbd732f8
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-08 14:48:19 +0200

    snack 4

--- snack4/script.js
+++ snack4/script.js
@@ -2,20 +2,19 @@
 //     elementi) prendendo alternativamente gli elementi da uno e dall’altro
 //     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
-function creaArrey(arg1, arg2){
+function creaArrey(arr1, arr2){
     var nuovoArrey=[];
-    for(var i=0;i<arg1.lenght;i++){
-        nuovoArrey.push(lettere);
+    for(var i=0;i<arr1.lenght;i++){
+        nuovoArrey.push(arr1[i]);
+        nuovoArrey.push(arr2[i]);
     }
-    for (var i=0;i<arg2.lenght;i++){
-        nuovoArray.push(numeri);
-    }
+    return nuovoArrey;
 }
 
-var nuovoArrey=[];
-var lettere=["a","b","c"];
-var numeri=[1,2,3];
+
+var arr1=["a","b","c"];
+var arr2=[1,2,3];
 
 
-creaArrey(lettere,numeri);
-console.log(nuovoArrey);
+
+console.log(creaArrey(arr1,arr2));
